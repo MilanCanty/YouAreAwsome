cd0e3be373efadf9a05e7c2d3156835bbd460db9
MilanCanty <milancanty@gmail.com>
2023-01-25 18:42:02 -0500

    homework 1.6

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -13,30 +13,55 @@ struct ContentView: View {
     var body: some View {
 
         
-        VStack{
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(maxWidth:.infinity)
-                .border(.orange, width: 1)
+        ZStack {
+            Rectangle()
+                .fill(
+                    Gradient(colors: [.white,.blue,.red])
+                )
+                .ignoresSafeArea()
+            VStack{
+                Text("You have skills")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(maxWidth:.infinity)
+                    //.border(.orange, width: 1)
+                    .padding()
+                  
+                Spacer()
+              
+                
+                
+                HStack {
+                    Button("Awsome") {
+                        //this is the action preformed when the button is pressed
+                        messageString = "You Are Awsome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                //.border(.purple, width: 5)
                 .padding()
                 
-            
-            HStack {
-                Button("Awsome") {
-                    //this is the action preformed when the button is pressed
-                    messageString = "You Are Awsome!"
-                }
-                .buttonStyle(.borderedProminent)
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
             }
-            .border(.purple, width: 5)
         }
     }
 }
