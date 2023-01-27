69a181131726205ae4e1d4257f410d42554bd62f
MilanCanty <milancanty@gmail.com>
2023-01-27 10:15:52 -0500

    homework1.7

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -11,60 +11,59 @@ struct ContentView: View {
     @State private var messageString = "When the genious bar needs help they  call you " //comment
     
     var body: some View {
-
         
-        ZStack {
-            Rectangle()
-                .fill(
-                    Gradient(colors: [.white,.blue,.red])
-                )
-                .ignoresSafeArea()
-            VStack{
-                Text("You have skills")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .cornerRadius(15)
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(maxWidth:.infinity)
-                    //.border(.orange, width: 1)
-                    .padding()
-                  
-                Spacer()
-              
-                
-                
-                HStack {
-                    Button("Awsome") {
-                        //this is the action preformed when the button is pressed
-                        messageString = "You Are Awsome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                //.border(.purple, width: 5)
-                .padding()
-                
+        
+        
+        VStack{
+            
+            Group {
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
+                Text("I am a Text View")
             }
+            .font(.title2)
+            .fontWeight(<#T##weight: Font.Weight?##Font.Weight?#>)
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(maxWidth:.infinity)
+            //.border(.orange, width: 1)
+                .padding()
+            
+            Spacer()
+            
+            
+            HStack {
+                Button("Awsome") {
+                    //this is the action preformed when the button is pressed
+                    messageString = "You Are Awsome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            //.border(.purple, width: 5)
+            .padding()
+            
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
